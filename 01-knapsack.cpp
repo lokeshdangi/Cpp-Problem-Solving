47afe587dec953079513e9b7941462bc85c4b413
lokeshdangi <lokeshdangi002@gmail.com>
2018-06-30 11:37:32 +0530

    commit-3

---
+++ 01-knapsack.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+	int sack,n;
+	cout << "Enter sack size";
+	cin >> sack;
+	cout <<"Enter size: ";
+	cin >>n;
+	n+=1;
+	int mat[n][sack +1];
+	int temp;
+	int weight[n],value[n];
+	weight[0] = 0;
+	value[0] = 0;
+	
+	for(int i = 1;i < n;i++){
+		cout <<"Enter weight";
+		cin >> weight[i];
+	}
+	
+	for(int i = 1;i < n;i++){
+		cout <<"Enter value";
+		cin >> value[i];
+	}	
+	for(int i =0;i < n; i++){
+		mat[i][0] = 0;
+	}
+	for(int i = 0;i < sack +1;i++){
+		mat[0][i] = 0;
+	}
+	for(int i = 1; i < n; i++){
+		for(int j = 1; j < sack +1;j++){
+			if(j  >= weight[i]){
+				mat[i][j] = std::max(mat[i-1][j],mat[i-1][j - weight[i]] + value[i]); 
+			}
+			else{
+				 
+				mat[i][j] = mat[i-1][j];
+			}
+		}
+	}
+	cout <<"WHOLE MAT"<<endl;
+	for(int i = 0;i < n;i++){
+		for(int j = 0; j < sack +1; j++){
+			cout << mat[i][j]<<",";
+		}
+		cout << endl;
+	}
+	return 0;
+}
